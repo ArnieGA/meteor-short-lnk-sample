40b387f3257239dbde689f452fe61f3545bbc91d
Arnie Gonzalez <gonzalez.a.dev@gmail.com>
2017-05-28 13:02:59 -0400

    Updated project to METEOR@1.4.4.3. Added atmosphere packages for social login authentication (not yet configured/programmed). Updated the links api and the Links component for Url validation: Links component includes user-selectable list of protocols and the api validates everything.

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,8 +1,21 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
-
+import SimpleSchema from 'simpl-schema';
 export const LinksDb = new Mongo.Collection('links');
 
+// Extending SimpleSchema.RegEx with my own regexes:
+// SimpleSchema.RegEx.UrlHost = new RegExp(/^((\w+\.)+([a-z]{3,5}))|((\d{1,3}\.){3}\d{1,3}(\:([0-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-6]))?)$/i);
+// SimpleSchema.RegEx.UrlProtocol = new RegExp(/^(?:(?:https?|ftp)\:\/\/)$/i);
+// SimpleSchema.RegEx.PortRange = new RegExp(/^([0-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-6])$/);
+SimpleSchema.RegEx.Url2 = new RegExp(/^((?:https?|ftp)\:\/\/)(((\w+\.)+([a-z]{3,5}))|((\d{1,3}\.){3}\d{1,3}(\:([0-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-6]))?))$/i);
+// Defining the urlSchema for later use
+const urlSchema = new SimpleSchema({
+    url: {
+        type: String,
+        label: 'The URL you entered',
+        regEx: SimpleSchema.RegEx.Url2
+    }
+});
 // If the Meteor autopublish package is still installed, every user
 // will have live access to the database information.
 // After removing the autopublish package, the following
@@ -19,12 +32,20 @@ if(Meteor.isServer){
 }
 
 Meteor.methods({
-    'links.insert'(url){
-        if(!this.userId){
+    'links.insert'(protocol, host) {
+        if (!this.userId) {
             throw new Meteor.error('auth-error');
         }
+        // Perform the validation
+        try {
+            urlSchema.validate({ url: protocol + host });
+        } catch (e) {
+            // Validation falied, throw error
+            throw new Meteor.Error(400, e.message);
+        }
+        // Validation passed, write to the db
         LinksDb.insert({
-            url,
+            url: protocol + host,
             userId: this.userId
         });
     }
--- imports/react/components/pages/Links.jsx
+++ imports/react/components/pages/Links.jsx
@@ -17,10 +17,12 @@ class Links extends React.Component {
     }
     onSubmit(e){
         e.preventDefault();
-        const url = this.refs.url.value.trim();
-        if(!!url){
-            Meteor.call('links.insert', url);
-            this.refs.url.value = '';
+        const host = this.refs.host.value.trim();
+        if(!!host){
+            const selectedIndex = this.refs.protocols.options.selectedIndex;
+            const selectedProtocol = this.refs.protocols.options[selectedIndex].text;
+            Meteor.call('links.insert', selectedProtocol, host);
+            this.refs.host.value = '';
         }
     }
     render(){
@@ -31,7 +33,12 @@ class Links extends React.Component {
                 <LinksList />
                 <p>Add Link</p>
                 <form onSubmit={this.onSubmit}>
-                    <input type="text" ref="url" placeholder="URL"/>
+                    <select name="protocols" ref="protocols">
+                        <option value="http">http://</option>
+                        <option value="https">https://</option>
+                        <option value="ftp">ftp://</option>
+                    </select>
+                    <input type="text" ref="host" placeholder="URL host (address)"/>
                     <button>Add Link</button>
                 </form>
             </div>
