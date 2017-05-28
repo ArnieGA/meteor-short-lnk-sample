bd9f62a83966e80f268ba7d301c0622fd009830b
Arnie Gonzalez <gonzalez.a.dev@gmail.com>
2017-05-28 19:51:25 -0400

    Lesson 82 changes. Added the clipboard package in order to be able to copy to the clipboard. Added the copy button functionality to LinkItem.

--- imports/react/components/app-specific/LinkItem.jsx
+++ imports/react/components/app-specific/LinkItem.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Clipboard from 'clipboard';//<-- not a react library.
 
 export default class LinkItem extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            copied: false
+        };
+    }
+    componentDidMount(){
+        this.clipboard = new Clipboard(this.refs.copyButton);
+        this.clipboard.on('success', (e)=>{
+            this.setState({copied: true});
+            setTimeout(()=>this.setState({copied: false}),1000);
+        })
+        .on('error', (e)=>{
+            console.log('failure :\'(', e);
+        });
+    }
+    componentWillUnmount(){
+        this.clipboard.destroy();
     }
     render(){
         return(
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
+                <button ref="copyButton" data-clipboard-text={this.props.shortUrl}>{this.state.copied ? 'Copied' : 'Copy'}</button>
             </div>
         );
     }
