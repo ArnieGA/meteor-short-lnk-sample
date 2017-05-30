1ce1f33948298226c10ab24f723e3cc019659f5c
Arnie Gonzalez <gonzalez.a.dev@gmail.com>
2017-05-30 15:40:35 -0400

    Lecture 90: Added react-modal to handle modals. (Custom: added node-uuid to generate unique ids for the protocols selector)

--- imports/react/components/app-specific/AddLink.jsx
+++ imports/react/components/app-specific/AddLink.jsx
@@ -1,45 +1,61 @@
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import Modal from 'react-modal';
+import uuid from 'node-uuid';
+
+const allowedProtocols = ['http', 'https', 'ftp'];
 
 export default class AddLink extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            typedUrl: ''
+            typedUrl: '',
+            openModal: false
         };
         this.onSubmit = this.onSubmit.bind(this);
-        this.onTypedUrlChange = this.onTypedUrlChange.bind(this);     
+        this.onTypedUrlChange = this.onTypedUrlChange.bind(this);
     }
-    onSubmit(e){
+    onSubmit(e) {
         e.preventDefault();
         const host = this.state.typedUrl;
-        if(!!host){
+        if (!!host) {
             const selectedIndex = this.refs.protocols.options.selectedIndex;
             const selectedProtocol = this.refs.protocols.options[selectedIndex].text;
-            Meteor.call('links.insert', selectedProtocol, host, (err, response)=>{
-                if(!err) { this.setState({typedUrl: ''}); }
-                else { this.refs.host.select(); }
+            Meteor.call('links.insert', selectedProtocol, host, (err, response) => {
+                if (!err) {
+                    this.setState({ typedUrl: '', openModal: false });
+                }
+                else {
+                    this.refs.host.select();
+                }
             });
         }
     }
-    onTypedUrlChange(e){
-        this.setState({typedUrl: e.target.value.trim()});
+    onTypedUrlChange(e) {
+        this.setState({ typedUrl: e.target.value.trim() });
     }
-    render(){
-        return(
+    renderProtocolOptions() {
+        return allowedProtocols.map((protocol) => {
+            return <option key={`protocol_${uuid()}`} value={protocol}>{`${protocol}://`}</option>
+        });
+    }
+    render() {
+        return (
             <div>
-                <p>Add Link</p>
-                <form onSubmit={this.onSubmit}>
-                    <select name="protocols" ref="protocols">
-                        <option value="http">http://</option>
-                        <option value="https">https://</option>
-                        <option value="ftp">ftp://</option>
-                    </select>
-                    <input type="text" ref="host" placeholder="URL host (address)" 
-                        value={this.state.typedUrl}
-                        onChange={this.onTypedUrlChange} />
-                    <button>Add Link</button>
-                </form>
+                <button onClick={() => this.setState({ openModal: true })}>+ Add Link</button>
+                <Modal isOpen={this.state.openModal} contentLabel='Add link'>
+                    <p>Add Link</p>
+                    <form onSubmit={this.onSubmit}>
+                        <select name="protocols" ref="protocols">
+                            {this.renderProtocolOptions()}
+                        </select>
+                        <input type="text" ref="host" placeholder="URL host (address)"
+                            value={this.state.typedUrl}
+                            onChange={this.onTypedUrlChange} />
+                        <button>Add Link</button>
+                    </form>
+                    <button onClick={() => this.setState({ openModal: false, typedUrl: '' })}>Cancel</button>
+                </Modal>
             </div>
         );
     }
--- imports/react/components/pages/Links.jsx
+++ imports/react/components/pages/Links.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import PrivateHeader from '/imports/react/components/app-specific/PrivateHeader';
 import LinksListFilters from '/imports/react/components/app-specific/LinksListFilters';
-import LinksList from '/imports/react/components/app-specific/LinksList';
 import AddLink from '/imports/react/components/app-specific/AddLink';
+import LinksList from '/imports/react/components/app-specific/LinksList';
 
 export default () => {
     return (
         <div>
             <PrivateHeader title='Your Links' />
             <LinksListFilters />
-            <LinksList />
             <AddLink />
+            <LinksList />
         </div>
     );
 };
